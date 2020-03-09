0ab6f93671ad04f041924e354cf521746cd70620
binding-archive-7591 <yarhumher@hotmail.com>
2020-03-09 21:38:24 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,7 +1,8 @@
 class Book
   attr_accessor :author, :page_count
-  attr_reader :title, :genre
+  attr_reader :title, :genre, :brand
    GENRES = []
+   BRANDS = []
   def initialize(title)
     @title = title
   end
@@ -12,6 +13,10 @@ class Book
   def genre=(genre)
     @genre = genre
     GENRES << genre
+  end
+  def brand=(brand)
+    @brand = brand
+    BRANDS << brand
   end
 
 end
