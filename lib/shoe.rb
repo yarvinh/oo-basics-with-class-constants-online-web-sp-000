4f75dab603dcab8b01b57577a550c3e8ef89ab8f
binding-archive-7591 <yarhumher@hotmail.com>
2020-03-09 21:52:27 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -4,6 +4,7 @@ class Shoe
   BRANDS = []
   def initialize(brand)
     @brand = brand
+    BRANDS << brand
   end
 
   def cobble
@@ -12,6 +13,6 @@ class Shoe
   end
   def brand=(brand)
     @brand = brand
-  puts   BRANDS << brand
+   BRANDS << brand
   end
 end
